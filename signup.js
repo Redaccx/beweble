69ace29cdfb13b5ec4d58e8a78263fa97d1971ee
Redaccx <105161552+Redaccx@users.noreply.github.com>
2022-05-09 23:55:53 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,37 +1,29 @@
+function SignUpFunction(){
+    email = document.getElementById("signup_email_field").value;
+    pswd = document.getElementById("signup_pass_field").value;
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+    firebase.auth().createUserWithEmailAndPassword(email, pswd).then(function(){
+        // console.log('User Created !! ');
 
-    <div class="signup_div">
-        <h3>SignUp</h3>
-        <input type="email" id="signup_email_field" placeholder="Enter Your Email Address ..." required />
-        <input type="password" id="signup_pass_field" placeholder="Enter Your Password ..." required />
-        <input type="text" id="signup_image_field" placeholder="Enter Your Profile photo URL" required />
-        <input type="text" id="signup_uname_field" placeholder="Enter Your Profile User Name: "required />
+        var user = firebase.auth().currentUser;
 
-        <button onclick="SignUpFunction()">Create Account</button>
+        user.updateProfile({
+            photoURL: document.getElementById("signup_image_field").value,
+            displayName: document.getElementById("signup_uname_field").value
+        });
 
-        <h3>Already have Account ?</h3>
-        <a href="signin">
-            <button id="signup">Login</button>
-        </a>
+    
+    }).catch(function(error){
+        window.alert(error.message);
+        console.log('Error !!!');
+    });
 
-    </div>
+}
 
+firebase.auth().onAuthStateChanged(user => {
+    if(user){
+        window.open('index.html', target="_self");
+        document.title = "PROFILE PAGE";
 
-<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-analytics.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-auth.js"></script>
-
-<script src="init.js"></script>
-
-
-<script src="signup.js"></script>
-</body>
-</html>
+    }
+})
