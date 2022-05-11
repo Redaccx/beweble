b0b64b00769c54aca8db2dc86089e25fbb2e6876
Redaccx <105161552+Redaccx@users.noreply.github.com>
2022-05-11 23:59:04 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -22,7 +22,7 @@ function SignUpFunction(){
 
 firebase.auth().onAuthStateChanged(user => {
     if(user){
-        window.open('index.html', target="_self");
+        window.open('index', target="_self");
         document.title = "PROFILE PAGE";
 
     }
