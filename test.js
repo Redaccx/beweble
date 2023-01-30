a6fc18adb3f740770e84183eaf3542e6990d4e17
Redaccx <105161552+Redaccx@users.noreply.github.com>
2023-01-30 09:52:17 +0200

    Update test.js

--- test.js
+++ test.js
@@ -9,12 +9,12 @@ firebase.auth().onAuthStateChanged(function(user)
     {
       if (snapshot.val())
       {
-        window.location.href = "index";
+        window.location.href = "main";
 
       }
       else
       {
-        window.location.href = "main";
+        window.location.href = "index";
       }
     });
 
